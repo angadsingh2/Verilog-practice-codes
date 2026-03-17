d58adf82c4a2ff91a1f754fd71dacd0a28cb79cf
Angad Singh <angad22070@iiitd.ac.in>
2026-03-17 10:18:31 +0530

    verilog code and testbench

---
+++ fulladder_using_halfadders/full_adder_tb.v
@@ -0,0 +1,16 @@
+module tb();
+reg a,b,cin; wire sum,carry;
+full_adder uut(a,b,c ,sum,carry);
+initial
+begin
+a=0;b=0;c=0; #10
+a=0;b=0;c=1; #10
+a=0;b=1;c=0; #10
+a=0;b=1;c=1; #10
+a=1;b=0;c=0; #10
+a=1;b=0;c=1; #10
+a=1;b=1;c=0; #10
+a=1;b=1;c=1; #10
+$finish;
+end
+endmodule
